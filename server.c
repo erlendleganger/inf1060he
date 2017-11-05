b66032a3a3a33c67e4e41025170edee949437dcc
Audun Leganger <audun.leganger@gmail.com>
2017-11-05 13:17:49 +0100

    Added loggin to server

--- server.c
+++ server.c
@@ -3,31 +3,35 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
 
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-int usage(int argc, char* argv[])  {
-  if (argc < 3) {
-    printf("Usage: %s [jobname] [port]\n", argv[0]);
-    return 1;
-  }
-  return 0;
+#include "mylog.h"
+int MYLOGLEVEL=MYLOGLEVEL_TRACE;
+
+char me[32];
+
+void usage()  {
+  printf("\nUsage: %s [pathJobFile] [port]\n\n", me);
 }
 
 int get_port(char* port_as_string, unsigned short* port)  {
+  MYLOG_DEBUG("start");
   char* endptr;
 
   int ret = strtol(port_as_string, &endptr, 10);
-  if (endptr == port_as_string && ret == 0) {
+  if (!strcmp(endptr, port_as_string) && ret == 0) {
     printf("[Port]-Argument has to be an integer.\n");
-    return -1;
+    usage();
+    MYLOG_DEBUG("premature exit");
+    exit(EXIT_FAILURE);
   }
-  else  {
-    *port = (unsigned short)  ret;
-    return 0;
-  }
+  MYLOG_DEBUG("end");
+  return 0;
+
 }
 
 
@@ -71,11 +75,64 @@ int main(int argc, char* argv[]) {
   close(sock);
   close(request_sock);
   */
-  argc++;
+  MYLOG_DEBUG("begin");
+  strcpy(me, argv[0]);
 
-  char jobName[64];
-  strcpy(jobName, argv[1]);
-  printf("jobName: %s\n", jobName);
+
+  if (argc != 3)  {
+    printf("Feil antall parametere.\n");
+    usage();
+    exit(EXIT_FAILURE);
+  }
+  char pathJobFile[64];
+  char portString[32];
+  strcpy(pathJobFile, argv[1]);
+  strcpy(portString, argv[2]);
+
+
+
+
+  MYLOG_DEBUG("pathJobFile = %s", pathJobFile);
   FILE* fptr;
-  fptr = fopen(jobName, "r");
+
+  if (strlen(pathJobFile) == 0) {
+    perror("Mottok intet filnavn\n");
+    exit(EXIT_FAILURE);
+  }
+
+  fptr = fopen(pathJobFile, "r");
+
+
+  char jobType;
+  int jobLength;
+  int lest;
+  int i = 0;
+
+  while (!feof(fptr))   {
+
+
+
+    fread(&jobType, sizeof(char), 1, fptr);
+    fread(&jobLength, sizeof(int), 1, fptr);
+    MYLOG_DEBUG("Jobtype: %c", jobType);
+    MYLOG_DEBUG("Joblength: %d", jobLength);
+
+    char* jobString = malloc(sizeof(char)*jobLength);
+    if (jobString == NULL)  {
+      MYLOG_DEBUG("Malloc failed");
+      exit(errno);
+    }
+
+    lest = fread(&jobString, sizeof(char), jobLength, fptr);
+    if (lest != jobLength)  {
+      MYLOG_DEBUG("fread failed, lest = %d, jobLength = %d", lest, jobLength);
+      exit(EXIT_FAILURE);
+    }
+    MYLOG_DEBUG("jobString = %s", jobString);
+    free(jobString);
+    //jobString[jobLength] = '\0';
+    //printf("Jobstring: %s\n", jobString);
+  }
+  MYLOG_DEBUG("end");
+
 }
