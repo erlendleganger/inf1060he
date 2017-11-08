c7be4cfac0be0f74527b35d7675eb9126c5468e6
Audun Leganger <audun.leganger@gmail.com>
2017-11-08 23:18:33 +0100

    Fixed protocol.h

---
+++ protocol.h
@@ -0,0 +1,10 @@
+#define CLOSE0_MASK 1<<31
+#define CLOSE1_MASK 1<<30
+#define CLOSE2_MASK 1<<29
+#define GET_1_JOB_MASK 1<<28
+#define GET_X_JOB_MASK 1<<27
+#define GET_ALL_JOB_MASK 1<<26
+#define NUMBER_JOBS_MASK
+
+#define CHECKSUM_MASK 31
+#define JOBTYPE_MASK 224
